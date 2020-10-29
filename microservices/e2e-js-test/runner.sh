7a7eaa18a140e281815d62a6d06e70b59e2d51f4
Abdulrahman A <abdulrahman.asiri.a@gmail.com>
2020-10-29 06:21:12 +0300

    unit-testing

--- microservices/e2e-js-test/runner.sh
+++ microservices/e2e-js-test/runner.sh
@@ -1,20 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 code=0
 testfile=$1
 files=$testfile
 
-echo $files
-
 if [ -z $testfile ]
 then
-  files=$(ls *_test.js)
-  #files=$(ls test/e2e/*_test.js)
+  files=$(ls /e2e-test/*_test.js)
 fi
 
 for test in $files
 do
-  node test/e2e/test_helper.js
   casperjs test $test
   ret=$?
   if [ ! $ret == "0" ]; then code=1; fi
